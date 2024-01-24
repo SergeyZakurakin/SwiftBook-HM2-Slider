98eba1842ccc1d531355ca1491121c1888839942
SergeyZakurakin <117861728+SergeyZakurakin@users.noreply.github.com>
2024-01-24 21:17:39 +0000

    Initial Commit

---
+++ SwiftBook HM2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBook HM2
+//
+//  Created by Sergey Zakurakin on 24/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
