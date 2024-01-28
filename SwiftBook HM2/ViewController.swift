025c2a2e68cd7e9cbbac2b56dbd668de85807b6e
SergeyZakurakin <117861728+SergeyZakurakin@users.noreply.github.com>
2024-01-28 00:20:42 +0000

    Setup Color View, Slider

--- SwiftBook HM2/ViewController.swift
+++ SwiftBook HM2/ViewController.swift
@@ -8,16 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var colorView: UIView!
+    
 
+    
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redTextField: UITextField!
+    @IBOutlet weak var greenTextField: UITextField!
+    @IBOutlet weak var blueTextField: UITextField!
+    
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redSlider.value = 0.0
+        greenSlider.value = 0.0
+        blueSlider.value = 0.0
+        
+        colorView.layer.cornerRadius = 30
+        colorView.backgroundColor = .black
         
     }
-
     
     
     
+    
+    @IBAction func SlidersAction(_ sender: UISlider) {
+        colorView.backgroundColor = UIColor(_colorLiteralRed: redSlider.value,
+                                            green: greenSlider.value,
+                                            blue: blueSlider.value, alpha: 1)
+        
+        
+    }
+    
+    
+    
+ 
     
     
 
